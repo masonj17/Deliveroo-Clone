94759adc461c2cc7ca67cba977fa96e69a4c4984
masonj17 <jordan_mason99@hotmail.co.uk>
2023-07-08 15:33:21 +0100

    fixed safe area view issues

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import { TailwindProvider } from "tailwindcss-react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
     </NavigationContainer>
   );
 }
