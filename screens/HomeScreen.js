dc43e6dcffcfbac992b8d3df4f6ab0922353a538
masonj17 <jordan_mason99@hotmail.co.uk>
2023-07-09 16:00:06 +0100

    fixed flex-1 issue by replacing <Text> to <View>

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../assets/favicon.png";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 const HomeScreen = () => {
   const navigation = useNavigation();
 
@@ -12,23 +14,23 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">
+    <SafeAreaView className="bg-white pt-5">
+      <View className="text-red-500">
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: "https://links.papareact.come/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
+        <View className="flex-row items-center mx-4 space-x-2">
+          <Image source={logo} className="h-7 w-7 bg-gray-400 p-4 rounded-full" />
 
-          <View>
+          <View className="flex-1">
             <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location</Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB" />
         </View>
-      </Text>
+      </View>
     </SafeAreaView>
   );
 };
